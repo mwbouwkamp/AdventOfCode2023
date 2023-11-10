9f6b0a0811b7698481d42a765f95bc6048600acb
Marco Bouwkamp <marco.bouwkamp@mendix.com>
2023-11-10 10:39:49 +0100

    Add project files.

---
+++ Utils/FileUtils.cs
@@ -0,0 +1,131 @@
+﻿namespace AdventOfCode2023;
+
+internal class FileUtils
+{
+    private static readonly String rootFolder = @"C:\Users\Marco.Bouwkamp\Documents\High code\AdventOfCode2023\Resources\";
+    private readonly string fileName;
+
+    public FileUtils(String fileName)
+    {
+        if (!File.Exists(fileName.Trim()))
+            throw new FileNotFoundException($"File does not exist: {rootFolder}{fileName}");
+
+        this.fileName = fileName.Trim();
+    }
+
+    /// <summary>Gets lines.</summary>
+    /// <returns>
+    ///   List of lines.
+    /// </returns>
+    public List<String> GetLines()
+    {
+        return File.ReadLines(fileName).ToList();
+    }
+
+    /// <summary>Gets strings, using a delimiter.</summary>
+    /// <param name="delimiter">The delimiter.</param>
+    /// <returns>
+    ///   List of strings.
+    /// </returns>
+    public List<String> GetString(String delimiter)
+    {
+        return File.ReadAllText(fileName)
+            .Split(delimiter)
+            .ToList();
+    }
+
+    /// <summary>Gets characters as string, using a delimiter.</summary>
+    /// <param name="delimiter">The delimiter.</param>
+    /// <returns>
+    ///   List of characters as string values.
+    /// </returns>
+    public List<String> GetCharactersAsString(String? delimiter = "")
+    {
+        return File.ReadAllText(fileName)
+            .Split(delimiter)
+            .ToList();
+    }
+
+    /// <summary>Gets characters as char, using a delimiter.</summary>
+    /// <param name="delimiter">The delimiter.</param>
+    /// <returns>
+    ///   List of characters as char values.
+    /// </returns>
+    public List<char> GetCharactersAsChar(String delimiter = "")
+    {
+        return GetParsedElements(delimiter, char.Parse);
+    }
+
+    /// <summary>Gets integers, using a delimiter.</summary>
+    /// <param name="delimiter">The delimiter.</param>
+    /// <returns>
+    ///   List of integers.
+    /// </returns>
+    public List<int> GetIntegers(String delimiter)
+    {
+        return GetParsedElements(delimiter, int.Parse);
+    }
+
+    /// <summary>Gets integers, using a delimiter.</summary>
+    /// <param name="delimiter">The delimiter.</param>
+    /// <returns>
+    ///   List of doubles.
+    /// </returns>
+    public List<double> GetDoubles(String delimiter)
+    {
+        return GetParsedElements(delimiter, double.Parse);
+    }
+
+
+    /// <summary>Gets parsed elements, using a delimiter.</summary>
+    /// <typeparam name="T">The type to parse to</typeparam>
+    /// <param name="delimiter">The delimiter.</param>
+    /// <param name="parser">The parser.</param>
+    /// <returns>
+    ///   List of parsed elements of type T.
+    /// </returns>
+    private List<T> GetParsedElements<T>(String delimiter, Func<string, T> parser)
+    {
+        return File.ReadAllText(fileName)
+            .Split(delimiter)
+            .Select(element => parser(element))
+            .ToList();
+    }
+
+    /// <summary>Gets character grid.</summary>
+    /// <param name="delimiter">The delimiter.</param>
+    /// <returns>
+    ///   Grid of type char.
+    /// </returns>
+    public Grid<char> GetCharGrid(String delimiter = "")
+    {
+        return new Grid<char>(Get2DList(delimiter, char.Parse));
+    }
+
+    /// <summary>Gets integer grid.</summary>
+    /// <param name="delimiter">The delimiter.</param>
+    /// <returns>
+    ///   Grid of type integer.
+    /// </returns>
+    public Grid<int> GetIntGrid(String delimiter = "")
+    {
+        return new Grid<int>(Get2DList(delimiter, int.Parse));
+    }
+
+    /// <summary>Gets a list of lists of elmements.</summary>
+    /// <typeparam name="T">The type to parse to</typeparam>
+    /// <param name="delimiter">The delimiter.</param>
+    /// <param name="parser">The parser.</param>
+    /// <returns>
+    ///   List of lists of elements of type T.
+    /// </returns>
+    public List<List<T>> Get2DList<T>(String delimiter, Func<string, T> parser)
+    {
+        return GetLines()
+            .Select(line => line
+                .Split(delimiter)
+                .Select(parser)
+                .ToList())
+            .ToList();
+    }
+}
---
+++ Utils/Grid.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode2023;
+
+internal class Grid<T>
+{
+    private readonly List<List<T>> grid;
+
+    public Grid(List<List<T>> grid) 
+    {
+        this.grid = grid;
+    }
+
+    /// <summary>Gets the element at a row and column.</summary>
+    /// <param name="row">The row.</param>
+    /// <param name="col">The column.</param>
+    /// <returns>
+    ///   element at row and column.
+    /// </returns>
+    public T GetElement(int row, int col)
+    {
+        return grid[row][col];
+    }
+
+    /// <summary>Sets the element at a row and column.</summary>
+    /// <param name="row">The row.</param>
+    /// <param name="col">The column.</param>
+    /// <param name="value">The value to set.</param>
+    public void SetElement(int row, int col, T value) {
+        grid[row][col] = value;
+    }
+}
