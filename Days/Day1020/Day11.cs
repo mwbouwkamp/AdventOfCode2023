c28a8d0ebb5432bb7fac6e32b5095255ebc10f85
Marco Bouwkamp <marco.bouwkamp@mendix.com>
2023-12-11 08:14:43 +0100

    Solved Day11b

--- Days/Day1020/Day11.cs
+++ Days/Day1020/Day11.cs
@@ -11,12 +11,57 @@ public class Day11 : Day
     {
         List<string> lines = new FileUtils(input).GetLines();
 
-        List<int> emptyRows = lines
-            .Select((line, index) => line.Replace(".", "").Length == 0 ? index : -1)
-            .Where(index => index > 0)
-            .ToList();
+        List<int> emptyRows = GetEmptyRowIndexes(lines);
 
         RectangleGrid<char> initialGrid = new(lines.Select(line => line.ToCharArray().ToList()).ToList(), '.');
+        List<int> emptyCols = GetEmptyColIndexes(initialGrid);
+
+        long NUMTOINSERT = 2;
+        GalaxyMap galaxyMap = GetGalaxyMap(emptyRows, initialGrid, emptyCols, NUMTOINSERT);
+
+        return GetLengths(galaxyMap).ToString();
+    }
+
+    private static long GetLengths(GalaxyMap galaxyMap)
+    {
+        long lengths = 0;
+        List<(long row, long col)> positions = galaxyMap.Positions;
+        for (int i = 0; i < positions.Count; i++)
+        {
+            for (int j = i + 1; j < positions.Count; j++)
+            {
+                lengths += Math.Abs(positions[i].row - positions[j].row) + Math.Abs(positions[i].col - positions[j].col);
+            }
+        }
+
+        return lengths;
+    }
+
+    private static GalaxyMap GetGalaxyMap(List<int> emptyRows, RectangleGrid<char> initialGrid, List<int> emptyCols, long NUMTOINSERT)
+    {
+        GalaxyMap galaxyMap = new();
+        for (int row = 0; row < initialGrid.Height; row++)
+        {
+            for (int col = 0; col < initialGrid.Width; col++)
+            {
+                if (initialGrid.GetElement(row, col) == '#')
+                {
+                    int rowInserts = emptyRows
+                        .Where(emptyRow => emptyRow < row)
+                        .Count();
+                    int colInserts = emptyCols
+                        .Where(emptyCol => emptyCol < col)
+                        .Count();
+                    galaxyMap.SetElement(row + rowInserts * (NUMTOINSERT - 1), col + colInserts * (NUMTOINSERT - 1));
+                }
+            }
+        }
+
+        return galaxyMap;
+    }
+
+    private static List<int> GetEmptyColIndexes(RectangleGrid<char> initialGrid)
+    {
         List<int> emptyCols = new();
         for (int col = 0; col < initialGrid.Width; col++)
         {
@@ -29,58 +74,34 @@ public class Day11 : Day
                     break;
                 }
             }
-            if (isEmpty) 
+            if (isEmpty)
                 emptyCols.Add(col);
         }
 
-        lines = lines
-            .Select(line =>
-            {
-                StringBuilder builder = new(line);
-                for (int col = 0; col < emptyCols.Count; col++)
-                {
-                    builder.Insert(emptyCols[col] + col, '.');
-                }
-                return builder.ToString();
-            })
+        return emptyCols;
+    }
+
+    private static List<int> GetEmptyRowIndexes(List<string> lines)
+    {
+        return lines
+            .Select((line, index) => line.Replace(".", "").Length == 0 ? index : -1)
+            .Where(index => index > 0)
             .ToList();
-
-        int newWidth = lines[0].Length;
-        for (int row = 0; row < emptyRows.Count; row++)
-        {
-            string newLine = new('.', newWidth);
-            lines.Insert(emptyRows[row] + row, newLine);
-        }
-
-        RectangleGrid<char> adaptedGrid = new(lines.Select(line => line.ToCharArray().ToList()).ToList(), '.');
-
-        SpotGrid<char> spotGrid = new('.');
-        for (int row = 0; row < adaptedGrid.Height; row++)
-        {
-            for (int col = 0; col < adaptedGrid.Width; col++)
-            {
-                if (adaptedGrid.GetElement(row, col) == '#')
-                    spotGrid.SetElement(row, col, '#');
-            }
-        }
-
-        int lengths = 0;
-        List<(int row, int col)> positions = spotGrid.Positions.Keys.ToList();
-        for (int i = 0; i < positions.Count; i++)
-        {
-            for (int j = i + 1; j< positions.Count; j++)
-            {
-                lengths += Math.Abs(positions[i].row - positions[j].row) + Math.Abs(positions[i].col - positions[j].col);
-            }
-        }
-
-        //High: 1490641232
-        return lengths.ToString();
     }
 
     public override string ExecuteB()
     {
-        throw new NotImplementedException();
+        List<string> lines = new FileUtils(input).GetLines();
+
+        List<int> emptyRows = GetEmptyRowIndexes(lines);
+
+        RectangleGrid<char> initialGrid = new(lines.Select(line => line.ToCharArray().ToList()).ToList(), '.');
+        List<int> emptyCols = GetEmptyColIndexes(initialGrid);
+
+        long NUMTOINSERT = 1000000;
+        GalaxyMap galaxyMap = GetGalaxyMap(emptyRows, initialGrid, emptyCols, NUMTOINSERT);
+
+        return GetLengths(galaxyMap).ToString();
     }
 
     public class GalaxyMap
--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿using AdventOfCode2023;
 
-Console.WriteLine(new DayExecuter("Day11", "input11.txt").ExecuteA());
+Console.WriteLine(new DayExecuter("Day11", "input11.txt").ExecuteB());
